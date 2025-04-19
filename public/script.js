992f11da38134c1fab78ca2c39797d32d73a86ca
dujimole <dlwltjd345@icloud.com>
2025-04-19 14:31:56 +0900

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -1,8 +1,16 @@
-function showHomepage() {
-  document.getElementById('video-screen').classList.add('hidden');
-  document.getElementById('homepage').classList.remove('hidden');
-}
-document.getElementById('start-btn').addEventListener('click', function() {
-  document.getElementById('intro').classList.add('hidden');
-  document.getElementById('video-screen').classList.remove('hidden');
+const startBtn = document.getElementById('start-btn');
+const intro = document.getElementById('intro');
+const videoSection = document.getElementById('video');
+const home = document.getElementById('home');
+const introVideo = document.getElementById('intro-video');
+
+startBtn.addEventListener('click', () => {
+  intro.classList.add('hidden');
+  videoSection.classList.remove('hidden');
+  introVideo.play();
+});
+
+introVideo.addEventListener('ended', () => {
+  videoSection.classList.add('hidden');
+  home.classList.remove('hidden');
 });
