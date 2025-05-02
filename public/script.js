805b3490d0e29f356d7211729d849bb5e6751327
dujimole <dlwltjd345@icloud.com>
2025-05-02 22:49:24 +0900

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -2,13 +2,11 @@ const btn = document.getElementById('startBtn');
 const intro = document.getElementById('intro');
 const video = document.getElementById('introVideo');
 const main = document.getElementById('main');
-
 btn.addEventListener('click', () => {
   intro.style.display = 'none';
   video.style.display = 'block';
   video.play();
 });
-
 video.addEventListener('ended', () => {
   video.style.display = 'none';
   main.style.display = 'block';
